4c17196c2ee211505917399108113e6e297c99a9
trushamaheshwari <trusha.m.ug23@plaksha.edu.in>
2025-02-26 20:08:14 +0530

    adding dockerfile

--- distance_classification.py
+++ distance_classification.py
@@ -1,2 +1,7 @@
-import numpy as np
 import pandas as pd
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+if __name__ == "__main__":
+    print("Dataset loading script initialized.")
